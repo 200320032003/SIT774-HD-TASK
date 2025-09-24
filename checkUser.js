c2166cfac93de59613339707b099fb6aaccfb804
Oshadhi03 <46785804+200320032003@users.noreply.github.com>
2025-09-24 14:35:08 +1000

    CheckUser.js

--- checkUser.js
+++ checkUser.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const sqlite3 = require('sqlite3').verbose();
 
 const db = new sqlite3.Database('./db/otazora.sqlite');
@@ -12,18 +12,3 @@ db.all('SELECT * FROM users', [], (err, rows) => {
 });
 
 db.close();
-=======
-const sqlite3 = require('sqlite3').verbose();
-
-const db = new sqlite3.Database('./db/otazora.sqlite');
-
-db.all('SELECT * FROM users', [], (err, rows) => {
-  if (err) {
-    throw err;
-  }
-  console.log("Users in database:");
-  console.log(rows);
-});
-
-db.close();
->>>>>>> 4c5ccae6bf4a1764a2582ba495180a709a428126
